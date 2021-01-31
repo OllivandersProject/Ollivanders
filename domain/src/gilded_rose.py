a29207f2c85b24296eceb611ea8de095732422de
pau <pllinas@cifpfbmoll.eu>
2021-01-31 22:52:21 +0100

    feat: :sparkles: Add GildedRose class

---
+++ domain/src/gilded_rose.py
@@ -0,0 +1,11 @@
+class GildedRose():
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_inventory():
+        for item in self.items:
+            inventory.update_inventory()
+
+    def getItems(self):
+        return self.items
