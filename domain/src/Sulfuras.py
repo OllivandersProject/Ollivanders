cc150f700e24ce3ea448c9aa2332d292c5cf76f3
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-01-31 02:18:12 +0100

    feat: :sparkles: Created Sulfura Class with getters and setters and update_quality() method override

---
+++ domain/src/Sulfuras.py
@@ -0,0 +1,43 @@
+
+# class Sulfuras(NormalItem):
+class Sulfuras():
+    
+    def __init__(self, name='', sell_in=0, quality=80):
+        # super().__init__(name, sell_in, quality)
+        
+        #* Contructor sin aplicar Herencia aún
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+        self.legendary = True
+        
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, name):
+        self.name = name
+    
+    def get_sell_in(self):
+        return self.sell_in
+    
+    def set_sell_in(self, sell_in):
+        self.sell_in = sell_in
+    
+    def get_quality(self):
+        return self.quality
+    
+    def set_quality(self, quality):
+        self.quality = quality
+    
+    def is_legendary(self):
+        return self.legendary
+    
+    def set_legendary(self, legendary):
+        self.legendary = legendary
+        
+    def udpate_quality(self):
+        
+        assert self.get_quality() == 80, f"quality de {self.__class__.__name__} distinta de 80"
+        pass
+    
+    
--- domain/test/test_sulfuras.py
+++ domain/test/test_sulfuras.py
@@ -1,9 +1,9 @@
 
-from src.Sulfuras import Sulfuras
+from domain.src.Sulfuras import Sulfuras
 import pytest
 
 def test_sulfuras_properties():
-    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)
+    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=0)
     
     assert sulfuraItem.get_name() == "Sulfuras, Hand of Ragnaros"
     assert sulfuraItem.get_sell_in() == 0
@@ -12,7 +12,7 @@ def test_sulfuras_properties():
     
 def test_sulfuras_day_one():
     
-    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=6, quality=80)
+    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=6)
     
     sulfuraItem.update_quality()
     
@@ -20,7 +20,7 @@ def test_sulfuras_day_one():
 
 def test_sulfuras_day_two():
     
-    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=5, quality=80)
+    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=5)
     
     sulfuraItem.update_quality()
     
@@ -28,7 +28,7 @@ def test_sulfuras_day_two():
     
 def test_sulfuras_day_three():
     
-    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=4, quality=80)
+    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=4)
     
     sulfuraItem.update_quality()
     
@@ -36,7 +36,7 @@ def test_sulfuras_day_three():
     
 def test_sulfuras_day_four():
     
-    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=3, quality=80)
+    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=3)
     
     sulfuraItem.update_quality()
     
@@ -44,7 +44,7 @@ def test_sulfuras_day_four():
     
 def test_sulfuras_day_five():
     
-    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=2, quality=80)
+    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=2)
     
     sulfuraItem.update_quality()
     
@@ -52,7 +52,7 @@ def test_sulfuras_day_five():
     
 def test_sulfuras_day_six():
     
-    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=1, quality=80)
+    sulfuraItem = Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=1)
     
     sulfuraItem.update_quality()
     
