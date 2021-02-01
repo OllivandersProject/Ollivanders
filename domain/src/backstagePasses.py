b273a305a402063be08d044d1cbb6889f5fadd28
pau <pllinas@cifpfbmoll.eu>
2021-02-01 01:40:22 +0100

    feat: :sparkles: Implementation getters and setters BackstagePasses class

--- domain/src/backstagePasses.py
+++ domain/src/backstagePasses.py
@@ -5,22 +5,40 @@ class BackstagePasses():
         self.sell_in = sell_in
         self.quality = quality
 
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_sell_in(self):
+        return self.sell_in
+
+    def set_sell_in(self, sell_in):
+        self.sell_in = sell_in
+        self.sell_in -= 1
+
     def get_quality(self):
         return self.quality
 
-    def update_quality(self):
-        if self.sell_in > 10:
-            if self.quality < 50:
-                self.quality += 1
-        elif self.sell_in > 5:
-            if self.quality < 49:
-                self.quality += 2
-            else:
-                self.quality = 50
-        elif self.sell_in > 0:
-            if self.quality < 48:
-                self.quality += 3
-            else:
-                self.quality = 50
+    def set_quality(self, value):
+
+        if self.quality + value > 50:
+            self.quality = 50
+        elif value > 0:
+            self.quality += value
         else:
             self.quality = 0
+        assert 0 <= self.quality <= 50, f"The quality of {self.__cla:__name__} overcome the maximun quality"
+
+    def update_quality(self):
+
+        if self.sell_in > 10:
+            self.set_quality(1)
+        elif self.sell_in > 5:
+            self.set_quality(2)
+        elif self.sell_in > 0:
+            self.set_quality(3)
+        else:
+            self.set_quality(0)
+        self.set_sell_in(self.get_sell_in())
