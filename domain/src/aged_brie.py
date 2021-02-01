da79719561252099cb86300e3a54a594e81448c0
pau <pllinas@cifpfbmoll.eu>
2021-02-01 01:14:31 +0100

    feat: :sparkles: Implementation getter and setter in AgedBrie class

--- domain/src/aged_brie.py
+++ domain/src/aged_brie.py
@@ -5,11 +5,37 @@ class AgedBrie():
         self.sell_in = sell_in
         self.quality = quality
 
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_sell_in(self):
+        return self.sell_in
+
+    def set_sell_in(self, sell_in):
+        self.sell_in = sell_in
+        self.sell_in -= 1
+
     def get_quality(self):
         return self.quality
 
-    def update_quality(self):
-        if self.sell_in > 0:
-            self.quality += 1
+    def set_quality(self, value):
+
+        if self.quality + value > 50:
+            self.quality = 50
         else:
-            self.quality += 2
+            self.quality += value
+        assert 0 <= self.quality <= 50, f"The quality of {self.__cla:__name__} overcome the maximun quality"
+
+    def update_quality(self):
+
+        if self.sell_in > 0:
+            self.set_quality(+1)
+        else:
+            self.set_quality(+2)
+        self.set_sell_in(self.get_sell_in())
+
+        def __str__(self):
+            return "***************Item*************** \n Name: %s,\n Sell in: %d,\n Quality: %d" % (self.get_name(), self.get_sell_in(), self.get_quality())
