fef48c51a665501f902e884054cc1d0750cd091c
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-02-02 13:30:08 +0100

    refactor: :recycle: Adapting Sulfura Class to heredate NormalItem Class

--- domain/src/Sulfuras.py
+++ domain/src/Sulfuras.py
@@ -1,33 +1,10 @@
 
-# class Sulfuras(NormalItem):
-class Sulfuras():
+class Sulfuras(NormalItem):
     
     def __init__(self, name='', sell_in=0, quality=80):
-        # super().__init__(name, sell_in, quality)
         
-        #* Contructor sin aplicar Herencia aún
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
+        super().__init__(name, sell_in, quality)
         self.legendary = True
-        
-    def get_name(self):
-        return self.name
-    
-    def set_name(self, name):
-        self.name = name
-    
-    def get_sell_in(self):
-        return self.sell_in
-    
-    def set_sell_in(self, sell_in):
-        self.sell_in = sell_in
-    
-    def get_quality(self):
-        return self.quality
-    
-    def set_quality(self, quality):
-        self.quality = quality
     
     def is_legendary(self):
         return self.legendary
