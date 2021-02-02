04666f01e461597cfc9d5c3a26572d482e6e30d5
pau <pllinas@cifpfbmoll.eu>
2021-02-02 23:31:00 +0100

    style: :zap: Update metho update_quality NormalItem
    We set the value to be minus instead of pass a positive value

--- domain/src/normal_item.py
+++ domain/src/normal_item.py
@@ -22,17 +22,17 @@ class NormalItem():
         return self.quality
 
     def set_quality(self, value):
-        if self.quality - value > 0:
-            self.quality -= value
+        if self.quality + value > 0:
+            self.quality += value
         else:
             self.quality = 0
         assert 0 <= self.quality <= 50, f"The quality of {self.__class__.__name__} overcome the maximun quality"
 
     def update_quality(self):
         if self.sell_in > 0:
-            self.set_quality(1)
+            self.set_quality(-1)
         else:
-            self.set_quality(2)
+            self.set_quality(-2)
         self.set_sell_in(self.get_sell_in())
 
     def __str__(self):
